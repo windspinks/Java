40e66c89da07b84c0ecfc36acc1c3a888919d9d8
windspinks <windspinks@gmail.com>
2017-07-28 13:25:47 -0400

    WorkOrder getter/setter

--- src/com/WorkOrder.java
+++ src/com/WorkOrder.java
@@ -1,9 +1,50 @@
 package com;
 
 public class WorkOrder {
-    int id;
-    String description;
-    String senderName;
-    String status;
+    private int id;
+    private String description;
+    private String senderName;
+    private String status;
+    private static int currentID = 1;
 
+    public WorkOrder() {
+        id = currentID;
+        currentID++;
+    }
+
+    public WorkOrder(String description, String senderName, String status) {
+        this.description = description;
+        this.senderName = senderName;
+        this.status = status;
+        id = currentID;
+        currentID++;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getSenderName() {
+        return senderName;
+    }
+
+    public void setSenderName(String senderName) {
+        this.senderName = senderName;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
 }
