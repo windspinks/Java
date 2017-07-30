fbd9b7dc3adcae2a6663cbcb0d4c06c7f805e300
windspinks <windspinks@gmail.com>
2017-07-30 11:06:59 -0400

    made test for readIt() and modified Workorder to include toStrin() equals() and hashCode() overrides

--- src/com/WorkOrder.java
+++ src/com/WorkOrder.java
@@ -46,4 +46,37 @@ public class WorkOrder {
     public void setStatus(Status status) {
         this.status = status;
     }
+
+    @Override
+    public String toString() {
+        return "WorkOrder{" +
+            "id=" + id +
+            ", description='" + description + '\'' +
+            ", senderName='" + senderName + '\'' +
+            ", status=" + status +
+            '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        WorkOrder workOrder = (WorkOrder) o;
+
+        if (id != workOrder.id) return false;
+        if (description != null ? !description.equals(workOrder.description) : workOrder.description != null)
+            return false;
+        if (senderName != null ? !senderName.equals(workOrder.senderName) : workOrder.senderName != null) return false;
+        return status == workOrder.status;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + (description != null ? description.hashCode() : 0);
+        result = 31 * result + (senderName != null ? senderName.hashCode() : 0);
+        result = 31 * result + (status != null ? status.hashCode() : 0);
+        return result;
+    }
 }
